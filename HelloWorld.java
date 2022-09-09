4c4202751d274e44ad999480fac42365393cee8a
James <32552207+dev23jjl@users.noreply.github.com>
2022-09-09 21:38:52 +0000

    Hello World! - it works!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,15 @@
-
+/* ****************************************************************************
+*
+* James Johanningmeier        DLM: 9/9/2022       HelloWorld.java
+*
+*Description: These are the basic required components of a simple Java program
+*                       This program prints Hello world! to the screen
+*
+*******************************************************************************
+*/
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
