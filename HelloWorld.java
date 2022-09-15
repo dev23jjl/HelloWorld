bdca09e0d7e5d6bef090824b55c02f8c378193c9
James <32552207+dev23jjl@users.noreply.github.com>
2022-09-15 00:04:36 +0000

    Better Formatting of HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,12 @@
 *
 * James Johanningmeier        DLM: 9/9/2022       HelloWorld.java
 *
-*Description: These are the basic required components of a simple Java program
-*                       This program prints Hello world! to the screen
+* Description: These are the basic required components of a simple Java program
+*               This program prints Hello world! to the screen
 *
 *******************************************************************************
 */
+
 public class HelloWorld {
     public static void main(String[] args)
     {
