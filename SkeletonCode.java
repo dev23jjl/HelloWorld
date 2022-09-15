1faa22cb6a5f2450ee19ec9c408772602834f367
James <32552207+dev23jjl@users.noreply.github.com>
2022-09-15 00:05:44 +0000

    SkeletonCode.java

---
+++ SkeletonCode.java
@@ -0,0 +1,16 @@
+/* ************************************************************************
+*
+* James Johanningmeier          DLM: 9/14/22            SkeletonCode.java
+*
+* Description: these are the basic required components of a simple Java program
+*               this program prints Skeleton code to the screen
+*
+************************************************************************
+*/
+
+public class SkeletonCode {
+    public static void main(String[] args) 
+    {
+        System.out.println("Skeleton Code!");
+    }
+}
