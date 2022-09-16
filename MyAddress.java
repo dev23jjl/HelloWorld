a5fd353b347c514949b08c77b486d90c3f596a74
James <32552207+dev23jjl@users.noreply.github.com>
2022-09-16 21:08:41 +0000

    MyAddress.java completed!

---
+++ MyAddress.java
@@ -0,0 +1,8 @@
+public class MyAddress {
+    public static void main(String[] args) 
+    {
+        System.out.println("James Johanningmeier");
+        System.out.println("965 Highway 76");
+        System.out.println("Waukon, IA 52172");
+    }
+}
