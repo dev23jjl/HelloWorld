f4134ae7cd78b68cb7978d22a438a178be89b5d5
James <32552207+dev23jjl@users.noreply.github.com>
2022-09-22 00:09:03 +0000

    Add playground for random ideas to test from the lesson

---
+++ Playground.java
@@ -0,0 +1,7 @@
+public class Playground {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the playground");
+        System.out.println("This is where you can test ideas that you come across in the lesson!");
+    }
+}
