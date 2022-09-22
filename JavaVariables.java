73ef7523d9a4b87b864cf53fb4f1360044ef90b6
James <32552207+dev23jjl@users.noreply.github.com>
2022-09-22 21:06:15 +0000

    Update code, add file description

--- JavaVariables.java
+++ JavaVariables.java
@@ -1,3 +1,16 @@
+/* 
+*********************************************************************************
+ *       James Johanningmeier     DLM: 9/22/2022       JavaVariables.java 
+ * 
+ * Description: These are the basic required components of a simple Java program
+ *  This program initializes five variables with descriptive names (that have 
+ *    incorrect values), prints their name and their value in the terminal, 
+ *      changes the value of the variables to the correct values using 
+ *   mathematical operations, and prints out the new values in the terminal
+ *  
+********************************************************************************* 
+*/
+
 public class JavaVariables {
     public static void main(String[] javaVar)
     {
@@ -9,17 +22,20 @@ public class JavaVariables {
         int daysOfWeek = 24;
 
         // Print out values of each variable
+        System.out.println("-------------------------------------------------------");
         System.out.println("daysOfYear= " + daysOfYear);
         System.out.println("weeksOfYear= " + weeksOfYear);
         System.out.println("monthsOfYear= " + monthsOfYear);
         System.out.println("daysOfWeek= " + daysOfWeek);
         System.out.println("hoursOfDay= " + hoursOfDay);
+        System.out.println("");
         System.out.println("Wow, someone got something wrong here - let's fix it...");
+        System.out.println("-------------------------------------------------------");
 
-        //Replace current values
+        //Replace current values by performing mathematical operations
         daysOfYear = monthsOfYear * daysOfYear + 1; //Now 365
         weeksOfYear = daysOfYear / monthsOfYear; //Now 52
-        monthsOfYear = daysOfWeek / 2; //Now 12
+        monthsOfYear = daysOfWeek - hoursOfDay; //Now 12
         hoursOfDay = monthsOfYear * 2; //Now 24
         daysOfWeek = daysOfYear / weeksOfYear; //Now 7
 
@@ -29,6 +45,8 @@ public class JavaVariables {
         System.out.println("monthsOfYear= " + monthsOfYear);
         System.out.println("daysOfWeek= " + daysOfWeek);
         System.out.println("hoursOfDay= " + hoursOfDay);
+        System.out.println("");
         System.out.println("All better! :)");
+        System.out.println("-------------------------------------------------------");
     }
 }
